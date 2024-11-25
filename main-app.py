3aa74ad671f0a45112d7ecc87d8c08e93c212a67
bigdefence <wjdrkdqls12@naver.com>
2024-11-26 01:19:19 +0900

    Update main-app.py

--- main-app.py
+++ main-app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify, stream_with_context,
 from dotenv import load_dotenv
 import asyncio
 import json
-from services.search import fetch_all_search_results, fetch_and_process_content, semantic_search_with_click_data
+from services.search import fetch_all_search_results, fetch_and_process_content, semantic_search
 from utils.database import init_db, increment_click_count
 from services.models import chatgpt_query, gemini_query
 from utils.text_processing import process_query
@@ -10,13 +10,6 @@ from utils.text_processing import process_query
 load_dotenv()
 
 app = Flask(__name__)
-
-@app.route('/record_click', methods=['POST'])
-def record_click():
-    data = request.get_json()
-    url = data['url']
-    increment_click_count(url)
-    return '', 204
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -28,7 +21,7 @@ def index():
         text_results, image_results, video_results = asyncio.run(fetch_all_search_results(optimized_query))
         processed_text_results = asyncio.run(fetch_and_process_content(text_results))
 
-        top_indices = semantic_search_with_click_data(optimized_query,processed_text_results)
+        top_indices = semantic_search(optimized_query, processed_text_results)
         prompt = f"Search results:\n"
         for i, index in enumerate(top_indices, 1):
             result = processed_text_results[index]
@@ -47,6 +40,23 @@ def index():
                 'snippet': result.get('snippet', 'No Snippet')[:200] + '...' if len(result.get('snippet', '')) > 200 else result.get('snippet', 'No Snippet'),
                 'link': result.get('link', 'No Link'),
             })
+
+        image_results_processed = [
+            {
+                "link": image.get('link'),
+                "thumbnail": image.get('image'),
+                "title": image.get('title')
+            } for image in image_results
+        ]
+
+        video_results_processed = [
+            {
+                "link": video.get('link'),
+                "thumbnail": video.get('thumbnail'),
+                "title": video.get('title')
+            } for video in video_results
+        ]
+
         def generate():
             ai_response = ""
             if model_choice == 'ChatGPT':
@@ -57,19 +67,19 @@ def index():
                 for chunk in gemini_query(prompt, optimized_query):
                     ai_response += chunk
                     yield chunk
-            
+
             final_response = {
-                "ai_response": ai_response,
-                "search_results": display_results,
-                "image_results": image_results,
-                "video_results": video_results
+                "image_results": image_results_processed,
+                "video_results": video_results_processed,
+                "search_results": display_results
             }
-            yield "\n" + json.dumps(final_response)
+            # JSON 데이터를 별도로 전송
+            yield json.dumps(final_response)
+
 
         return Response(stream_with_context(generate()), content_type='application/json')
 
     return render_template('index.html')
 
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True)
